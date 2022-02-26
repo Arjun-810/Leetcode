affed2a54b18fff9e08d71996daf6dad9b9a8781
arjun kaushik <arjunkaushik810@gmail.com>
2022-02-26 16:16:15 +0530

    lineToString

--- leetCode/LinesToString.java
+++ leetCode/LinesToString.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 
 public class LinesToString {
     public static int[] numberOfLines(int[] widths, String s) {
-        return widths;
+        int score=0,f=1;
+        for(int i=0;i<s.length();i++){
+            int a = s.charAt(i)-'a';
+            if (score==100 || score+widths[a]>100) {
+                score = 0;
+                f++;
+            }
+            score += widths[a];
+        }
+        return new int[]{f, score};
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
