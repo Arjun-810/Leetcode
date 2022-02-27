4bb775f5435f136bbf339349e90407cda08f573c
arjun kaushik <arjunkaushik810@gmail.com>
2022-02-27 09:33:54 +0530

    consistentString

---
+++ leetCode/consistentString.java
@@ -0,0 +1,33 @@
+package leetCode;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class consistentString {
+    public static int countConsistentStrings(String allowed,String[] words) {
+        int n=0;
+        HashSet<Character> set = new HashSet<>();
+        for (char a:allowed.toCharArray())
+            set.add(a);
+        for (String w:words){
+            boolean f = true;
+            for (char a:w.toCharArray())
+                if (!set.contains(a))
+                    f=false;
+            if (f)
+                n++;
+        }
+        return n;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String allowed = sc.next();
+        int n = sc.nextInt();
+        String[] words = new String[n];
+        for (int i=0;i<n;i++)
+            words[i] = sc.next();
+        int x = countConsistentStrings(allowed, words);
+        System.out.println(x);
+    }
+}
