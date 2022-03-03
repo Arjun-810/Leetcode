41e025500058b9e19f0d5e0a2124c48b0430f35b
Arjun-810 <63454696+Arjun-810@users.noreply.github.com>
2022-03-03 09:10:28 +0530

    Time: 2 ms (27.99%), Space: 42.7 MB (9.04%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+               if (s1.length() != s2.length()) return false;
+        int n = s1.length();
+        int k=0,count=0;
+        char[] to_swap = new char[2];
+        int[] index = new int[2];
+        for (int i=0;i<n;i++){
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+            if (!(String.valueOf(c1).equals(String.valueOf(c2)))) {
+                if (k < 2) {
+                    to_swap[k] = s1.charAt(i);
+                    index[k] = i;
+                    k++;
+                }
+                count++;
+            }
+        }
+        if (count == 0) return true;
+        if (count!=2) return false;
+        return s2.charAt(index[0]) == to_swap[1] && s2.charAt(index[1]) == to_swap[0]? true:false;
+    }
+}
