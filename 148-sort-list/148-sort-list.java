dd1d958746ab32ed8efca1e5a1ebe7dc414734c7
Arjun-810 <63454696+Arjun-810@users.noreply.github.com>
2022-03-04 09:32:56 +0530

    Time: 20 ms (17.17%), Space: 57.5 MB (26.97%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -12,15 +12,13 @@ class Solution {
     public ListNode sortList(ListNode head) {
         ListNode temp = head;
         ArrayList<Integer> arr = new ArrayList<>();
-        int len  = 0;
         while(temp!= null){
             arr.add(temp.val);
-            len++;
             temp = temp.next;
         }
         Collections.sort(arr);
         temp = head;
-        for(int i=0;i<len;i++){
+        for(int i=0;i<arr.size();i++){
             temp.val = arr.get(i);
             temp = temp.next;
         }
