2e00bc882cf52759944b9f94a9fdb81e1b9300b9
Arjun-810 <63454696+Arjun-810@users.noreply.github.com>
2022-03-30 10:36:00 +0530

    Added solution - LeetHub

--- Check Mirror in N-ary tree - GFG/check-mirror-in-nary-tree.java
+++ Check Mirror in N-ary tree - GFG/check-mirror-in-nary-tree.java
@@ -40,27 +40,19 @@ class Solution {
     static int checkMirrorTree(int n, int e, int[] A, int[] B) {
         // code here
     
-          //Lists to store nodes of the tree
         List<Stack<Integer>> s = new ArrayList<>();
         List<Queue<Integer>> q = new ArrayList<>();
- 
-        // initializing both list with empty stack and queue
         for (int i = 0; i <= n; i++) {
             s.add(new Stack<>());
             Queue<Integer> queue = new LinkedList<>();
             q.add(queue);
         }
- 
-           // add all nodes of tree 1 to list of stack and tree 2 to list of queue
         for (int i = 0; i < 2 * e; i += 2) {
             s.get(A[i]).push(A[i + 1]);
             q.get(B[i]).add(B[i + 1]);
         }
  
-          // now take out the stack and queues
-        // for each of the nodes and compare them
-        // one by one
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             while (!s.get(i).isEmpty() && !q.get(i).isEmpty()) {
                 int a = s.get(i).pop();
                 int b = q.get(i).poll();
