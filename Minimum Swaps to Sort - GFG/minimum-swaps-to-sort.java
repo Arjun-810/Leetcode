90d45b2f27bb7f813b9a474fc2db02ec3431e2f7
Arjun-810 <63454696+Arjun-810@users.noreply.github.com>
2022-04-06 14:08:23 +0530

    Added solution - LeetHub

---
+++ Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.java
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] nums = new int[n];
+            String[] S = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++){
+                nums[i] = Integer.parseInt(S[i]);
+            }
+            Solution obj = new Solution();
+            int ans = obj.minSwaps(nums);
+            System.out.println(ans);
+       }
+    }
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to find the minimum number of swaps required to sort the array.
+    public int minSwaps(int nums[])
+    {
+        // Code here
+         int[] arr = Arrays.copyOfRange(nums, 0, nums.length);
+        Arrays.sort(arr);
+        int c = 0;
+        for(int i=0;i<nums.length;i++){
+            if(arr[i] != nums[i]){
+                c++;
+                swap(nums,i,indexOf(nums,arr[i]));
+            }
+        }
+        return c;
+    }
+   public void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+     public int indexOf(int[] arr, int ele)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] == ele) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
