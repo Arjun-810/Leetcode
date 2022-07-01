d3bcd599c69280a2d14bb3058885043dd6f1242f
Arjun-810 <63454696+Arjun-810@users.noreply.github.com>
2022-07-02 01:38:42 +0530

    Time: 759 ms (5.08%), Space: 45.2 MB (90.91%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,47 +11,17 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        // Set<ListNode> set = new HashSet<>();
-        // while(headA != null){
-        //     set.add(headA);
-        //     headA = headA.next;
-        // }
-        // while(headB!= null){
-        //     if(set.contains(headB)) return headB;
-        //     headB = headB.next;
-        // }
-        // return null;
-        int c1 = 0,c2 = 0;
-        ListNode temp = headA;
-        while(temp != null){
-            c1++;
-            temp = temp.next;
+        ListNode match = null;
+        while(headA != null){
+            ListNode temp = headB;
+            while(temp != null){
+                if(headA == temp){
+                    return headA;
+                }
+                temp = temp.next;
+            }
+            headA = headA.next;
         }
-        temp = headB;
-        while(temp != null){
-            c2++;
-            temp = temp.next;
-        }
-        ListNode bigHead = null;
-        ListNode smallHead = null;
-        int diff = 0;
-        if(c1<c2){
-            diff = c2-c1;
-            bigHead = headB;
-            smallHead = headA;
-        }else{
-            diff = c1-c2;
-            bigHead = headA;
-            smallHead = headB;
-        }
-        for(int i=0;i<diff;i++)
-            bigHead = bigHead.next;
-        while(bigHead != null && smallHead != null){
-            if(bigHead == smallHead)
-                return bigHead;
-            smallHead = smallHead.next;
-            bigHead = bigHead.next;
-        }
-        return null;
+        return match;
     }
 }
