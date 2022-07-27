ca59a718ec54c850c740e0391a7a6d5ac54d2719
Arjun-810 <63454696+Arjun-810@users.noreply.github.com>
2022-07-28 01:25:06 +0530

    Time: 20 ms (27.40%), Space: 48.2 MB (5.37%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
         char[] sc = s.toCharArray();
         char[] tc = t.toCharArray();
         Arrays.sort(sc);
