72cc8c08c7b18a1acbb97eba29c3e0df737c81f8
Arjun-810 <63454696+Arjun-810@users.noreply.github.com>
2022-08-03 00:34:08 +0530

    Time: 1 ms (89.95%), Space: 54.2 MB (24.68%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,29 +1,23 @@
 class Solution {
-    public boolean isBip(int node, int[] col, int[][] graph){
-        col[node] = 0;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(node);
-        while(!q.isEmpty()){
-            node = q.poll();
-            for(int a: graph[node]){
-                if(col[a] == -1){
-                    q.add(a);
-                    if(col[node] == 0) 
-                        col[a] = 1;
-                    else
-                        col[a] = 0;
-                }
-                else if(col[a] == col[node]) return false;
+    public boolean isBip(int node, int[] col, int[][] edges){
+        for(int a: edges[node]){
+            if(col[a] == -1){
+                col[a] = 1-col[node];
+                if(!isBip(a,col,edges)) return false;
             }
+            else if(col[a] == col[node]) return false;
         }
         return true;
     } 
-    public boolean isBipartite(int[][] graph) {
-        int[] col = new int[graph.length];
-        for(int i=0;i<graph.length;i++) col[i] = -1;
-        for(int i=0;i<graph.length;i++){
-            if(col[i] == -1)
-                if(!isBip(i,col,graph)) return false;
+    public boolean isBipartite(int[][] edges) {
+        int n = edges.length;
+        int[] col = new int[n];
+        for(int i=0;i<n;i++) col[i] = -1;
+        for(int i=0;i<n;i++){
+            if(col[i] == -1){
+               col[i] = 0;
+               if(!isBip(i,col,edges)) return false;
+            }
         }
         return true;
     }
