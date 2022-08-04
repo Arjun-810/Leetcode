4d65af54d266e238411201c88e1e0631b9de6075
Arjun-810 <63454696+Arjun-810@users.noreply.github.com>
2022-08-04 21:32:43 +0530

    Time: 8 ms (36.92%), Space: 40.4 MB (76.40%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,13 +1,20 @@
 class Solution {
-    public int dpFib(int n, int[] arr){
+    // public int dpFib(int n, int[] arr){
+    //     if(n <=1) return n;
+    //     if(arr[n] != -1) return arr[n];
+    //     arr[n] = dpFib(n-1, arr) + dpFib(n-2, arr);
+    //     return arr[n];
+    // }
+    // public int fib(int n) {
+    //     int[] arr = new int[n+1];
+    //     for(int i=0;i<=n;i++) arr[i] =-1;
+    //     return dpFib(n, arr);
+    // }
+    public int dpFib(int n){
         if(n <=1) return n;
-        if(arr[n] != -1) return arr[n];
-        arr[n] = dpFib(n-1, arr) + dpFib(n-2, arr);
-        return arr[n];
+        return dpFib(n-1) + dpFib(n-2);
     }
     public int fib(int n) {
-        int[] arr = new int[n+1];
-        for(int i=0;i<=n;i++) arr[i] =-1;
-        return dpFib(n, arr);
+        return dpFib(n);
     }
 }
