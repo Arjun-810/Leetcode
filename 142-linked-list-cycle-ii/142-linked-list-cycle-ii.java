09978e437f87e9310680b756101498d3facad7f6
Arjun-810 <63454696+Arjun-810@users.noreply.github.com>
2022-08-08 01:28:29 +0530

    Time: 0 ms (100.00%), Space: 43 MB (88.82%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -10,14 +10,22 @@
  * }
  */
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
-        while(head != null){
-            if(set.contains(head)) return head;
-            set.add(head);
-            head = head.next;
+    public ListNode detectCycle(ListNode slow) {
+        if(slow == null) return null;
+        ListNode fast = slow;
+        ListNode entry = slow;
+         while(fast.next != null&&fast.next.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+            
+        if(slow == fast) {
+            while(slow != entry) {
+                slow = slow.next;
+                entry = entry.next;
+            }
+            return slow;
         }
-        return null;
     }
-    
+    return null;
+    }
 }
