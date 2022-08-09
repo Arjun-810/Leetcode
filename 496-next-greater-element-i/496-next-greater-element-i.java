f039b29f7e2e16a7c601bdc739838d55513e70a0
Arjun-810 <63454696+Arjun-810@users.noreply.github.com>
2022-08-09 23:43:01 +0530

    Time: 6 ms (53.23%), Space: 43.9 MB (68.36%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,11 +1,17 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] res = new int[nums1.length];
-        for (int i=0;i< nums1.length;i++){
-            res[i] = -1;
-            for (int j= nums2.length-1;j>=0;j--) {
-                if (nums2[j] > nums1[i]) res[i] = nums2[j];
-                if (nums1[i]==nums2[j]) break;
+        Arrays.fill(res,-1);
+        for(int i=0;i<nums1.length;i++){
+            boolean find = false;
+            for(int a: nums2){
+                if(a == nums1[i]) find = true;
+                if(find){
+                    if(a > nums1[i]){
+                        res[i] = a;
+                        break;
+                    }
+                }
             }
         }
         return res;
