9267953ddc01fd2ac45b5010ac19c09042901bd9
Arjun kaushik <63454696+Arjun-810@users.noreply.github.com>
2022-08-14 17:17:45 +0530

    Time: 8 ms (52.44%), Space: 49.8 MB (55.29%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,26 +9,29 @@
  */
 
 class Solution {
-    TreeNode common = null;
-    public void com(TreeNode root, TreeNode p, TreeNode q){
-        if(root == null) return;
-        if(root.val == p.val || root.val == q.val){
-            common = root;
-            return;
-        }
-        if(root.val < p.val && root.val < q.val) com(root.right,p,q);
-        else if(root.val >= p.val && root.val >= q.val) com(root.left,p,q);
-        else{ 
-            common  = root;
-            return;
-        }
+//     TreeNode common = null;
+//     public void com(TreeNode root, TreeNode p, TreeNode q){
+//         if(root == null) return;
+//         if(root.val == p.val || root.val == q.val){
+//             common = root;
+//             return;
+//         }
+//         if(root.val < p.val && root.val < q.val) com(root.right,p,q);
+//         else if(root.val >= p.val && root.val >= q.val) com(root.left,p,q);
+//         else{ 
+//             common  = root;
+//             return;
+//         }
         
-    }
+//     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null) return null;
-        common = root;
-        com(root,p,q);
-        return common;
+        if(root.val > p.val && root.val > q.val)
+            return lowestCommonAncestor(root.left,p,q);
+        else if(root.val < p.val && root.val < q.val)
+            return lowestCommonAncestor(root.right,p,q);
+        else return root;
+        // return root;
         
     }
 }
