d7c7e0562a2a7f52888c85d7aaddeed2b187e2f6
Arjun kaushik <63454696+Arjun-810@users.noreply.github.com>
2022-08-19 04:09:56 +0530

    Time: 174 ms (11.77%), Space: 109.9 MB (9.93%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,23 +1,39 @@
 class Solution {
-    public int subSeq(int idx,int[] nums,int prev,int[][] dp){
-        if(idx == nums.length){
-            return 0;
-        }
-        if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
-        int a = 0;
-        if(prev== -1 || ( nums[prev] < nums[idx])){
-            a = subSeq(idx+1, nums,idx,dp) + 1;
-        }
+//     public int subSeq(int idx,int[] nums,int prev,int[][] dp){
+//         if(idx == nums.length){
+//             return 0;
+//         }
+//         if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
+//         int a = 0;
+//         if(prev== -1 || ( nums[prev] < nums[idx])){
+//             a = subSeq(idx+1, nums,idx,dp) + 1;
+//         }
         
-        int b = subSeq(idx+1, nums,prev,dp)+0;
+//         int b = subSeq(idx+1, nums,prev,dp)+0;
         
-        return dp[idx][prev+1] = Math.max(a,b);
+//         return dp[idx][prev+1] = Math.max(a,b);
+//     }
+    
+    public int subSeq(int[] nums,int n,int[][] dp){
+        for(int idx = n-1;idx>=0;idx--){
+            for(int prev = idx-1;prev>=-1;prev--){
+                int a = 0;
+                if(prev== -1 || ( nums[prev] < nums[idx])){
+                    a = dp[idx+1][idx+1] + 1;
+                }
+        
+                int b = dp[idx+1][prev+1];
+                dp[idx][prev+1] = Math.max(a,b);
+            }
+        }
+        return dp[0][0];
     }
+    
     
     public int lengthOfLIS(int[] nums) {
         int[][] dp = new int[nums.length+1][nums.length+1];
         for(int[] a: dp)
-            Arrays.fill(a,-1);
-        return subSeq(0,nums,-1,dp);
+            Arrays.fill(a,0);
+        return subSeq(nums,nums.length,dp);
     }
 }
