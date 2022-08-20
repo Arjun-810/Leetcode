acc7f1e7d9febc14fef0984116186f5c507d90e8
Arjun kaushik <63454696+Arjun-810@users.noreply.github.com>
2022-08-20 23:35:57 +0530

    Time: 26 ms (33.34%), Space: 46.5 MB (37.29%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,7 +3,7 @@ class Solution {
         if(s_len == 0 || t_len == 0) return 0;
         if(dp[s_len-1][t_len-1] != -1) return dp[s_len-1][t_len-1];
         if(s.charAt(s_len-1) == t.charAt(t_len-1))
-            return 1+ find(s_len-1,s,t_len-1,t,dp);
+            return dp[s_len-1][t_len-1] = 1+ find(s_len-1,s,t_len-1,t,dp);
         
         return dp[s_len-1][t_len-1] = Math.max(find(s_len-1,s,t_len,t,dp) ,     find(s_len,s,t_len-1,t,dp));
     }
