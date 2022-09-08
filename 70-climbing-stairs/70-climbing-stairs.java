0e086becddb90bcfbd3d5532fd003a6290370497
Arjun kaushik <63454696+Arjun-810@users.noreply.github.com>
2022-09-08 18:43:56 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (59.44%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int rec(int n, int[] arr){
-        if(n<=2) return n;
-        if(arr[n] != -1) return arr[n];
-        // arr[n] =  ;
-        return arr[n] = rec(n-1,arr)+rec(n-2,arr);
+    public int rec(int n,int[] dp){
+        if(n<=2)
+            return n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = rec(n-1,dp)+rec(n-2,dp);
     }
     public int climbStairs(int n) {
-        // if(n<=2) return n;
-        // return climbStairs(n-1) + climbStairs(n-2);
-        int[] arr = new int[n+1];
-        for(int i=0;i<=n;i++) arr[i] = -1;
-        return rec(n, arr);
+        if(n <=2)
+            return n;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return rec(n,dp);
     }
 }
