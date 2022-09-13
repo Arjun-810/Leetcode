51e75ff67b0f031935cb0e0201bfd14efa3cade6
Arjun kaushik <63454696+Arjun-810@users.noreply.github.com>
2022-09-13 07:28:40 +0530

    Time: 1 ms (97.75%), Space: 44.8 MB (59.12%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,13 +1,30 @@
 class Solution {
     
     public int maxProduct(int[] nums) {
-        int max_so_far = nums[0],min_so_far = nums[0], res = nums[0];
-        for(int i=1;i<nums.length;i++){
-            int temp = Math.max(nums[i],Math.max(min_so_far*nums[i],max_so_far*nums[i]));
-        min_so_far = Math.min(nums[i],Math.min(min_so_far*nums[i],max_so_far*nums[i]));
-            max_so_far = temp;
-            res = Math.max(max_so_far,res);
+        int pro = 1;
+        int max_left = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            pro = pro*nums[i];
+            if(pro == 0){
+                pro= 1;
+                continue;
+            }
+            max_left = Math.max(max_left,pro);
         }
-        return res;
+        pro = 1;
+        int max_right = Integer.MIN_VALUE;
+        boolean zero = false; 
+        for(int i=nums.length-1;i>=0;i--){
+            pro = pro*nums[i];
+            if(pro == 0){
+                zero = true;
+                pro= 1;
+                continue;
+            }
+            max_right = Math.max(max_right,pro);
+        }
+        if(zero) 
+            return Math.max(0, Math.max(max_left,max_right));
+        return Math.max(max_left,max_right);
     }
 }
